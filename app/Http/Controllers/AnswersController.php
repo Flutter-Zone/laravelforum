38b1945b208b3f15928cbe49b50f5de9e785a61e
Flutter Zone <letsflatter@gmail.com>
2021-04-01 12:00:27 +0000

    Implement delete functionality for answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -66,8 +66,13 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Answer $answer)
+    public function destroy(Question $question, Answer $answer)
     {
         //
+        $this->authorize('delete', $answer);
+
+        $answer->delete();
+
+        return back()->with('success', 'YOur answer has been removed');
     }
 }
