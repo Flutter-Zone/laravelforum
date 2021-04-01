2fde6773b086fcd0d2e70388148464f884a1fc36
Flutter Zone <letsflatter@gmail.com>
2021-04-01 11:11:02 +0000

    Display answers for question

--- app/Answer.php
+++ app/Answer.php
@@ -6,31 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
-    public function question(){
-      return $this->belongsTo(Question::class);
-    }
+  //
+  protected $fillable = ['body', 'user_id'];
 
-    public function user(){
-      return $this->belongsTo(User::class);
-    }
+  public function question()
+  {
+    return $this->belongsTo(Question::class);
+  }
 
-    public function getBodyHtmlAttribute(){
-      return \Parsedown::instance()->text($this->body);
-    }
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 
-    public static function boot(){
-      parent::boot();
+  public function getBodyHtmlAttribute()
+  {
+    return \Parsedown::instance()->text($this->body);
+  }
 
-      static::created(function($answer){
-        $answer->question->increment('answers_count');
-        $answer->question->save();
-      });
-    }
+  public static function boot()
+  {
+    parent::boot();
 
-    public function getCreatedDateAttribute()
-    {
-      return $this->created_at->diffForHumans();
-    }
+    static::created(function ($answer) {
+      $answer->question->increment('answers_count');
+      $answer->question->save();
+    });
+  }
 
+  public function getCreatedDateAttribute()
+  {
+    return $this->created_at->diffForHumans();
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 // creating route for the QuestionsController. Exclude a route using the except
 Route::resource('questions', 'QuestionsController')->except('show');
+// Route::post('/questions/{question}/answres', 'AnswersController@store')->name('answers.store');
+Route::resource('questions.answers', 'AnswersController')->except(['index', 'create', 'show']);
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
